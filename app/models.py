2fa2c16fe2bfe5c62628ba418b5a5567bbc0bb34
e-keys <ekeysat@gmail.com>
2025-05-04 14:24:12 -0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), nullable=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     is_admin = db.Column(db.Boolean, default=False)
     nombre = db.Column(db.String(100))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
